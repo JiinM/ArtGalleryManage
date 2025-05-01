b65d2f5f2c222853be48b25ee27f5f12c1b33815
JiinM <jiinmin04@gmail.com>
2025-05-01 19:42:24 -0400

    Fix ALLOWED_HOSTS for Render deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n18py5&-^oqy_$__o64#ex^y2mnacr6#_@$qqg)#mx1rif!aok
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['artgallerymanage.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
